adf97e7a9349cb2759b189ed417914940cffe051
Bunleap Sorn <bunleap.sorn@gmail.com>
2022-07-29 10:08:58 +0700

    update pm scripts + .prettierrc

--- data/pm/index.js
+++ data/pm/index.js
@@ -1,115 +1,111 @@
 function setConfig(projectss, rotationss, handlingss) {
-  let config = {
-    projectsList: projectss
-      .replace(/(?:\r\n|\r|\n)/g, "")
-      .split(",")
-      .map(function (item) {
-        return parseInt(item);
-      }),
-    rotationss: parseInt(rotationss),
-    handlingss: parseInt(handlingss),
-  };
+    let config = {
+        projectsList: projectss
+            .replace(/(?:\r\n|\r|\n)/g, '')
+            .split(',')
+            .map(function (item) {
+                return parseInt(item);
+            }),
+        rotationss: parseInt(rotationss),
+        handlingss: parseInt(handlingss),
+    };
 
-  const projects = config.projectsList;
-  const rotation = config.rotationss;
-  const handle = config.handlingss;
+    const projects = config.projectsList;
+    const rotation = config.rotationss;
+    const handle = config.handlingss;
 
-  function findBA(projectsArray, projectRotation, projectHandling) {
-    let table = [
-      {
-        projects: 0,
-        multi: 0,
-        old_ba: 0,
-        new_ba: 0,
-        remain: 0,
-        need: 0,
-      },
-    ];
+    function findPM(projectsArray, projectRotation, projectHandling) {
+        let table = [
+            {
+                projects: 0,
+                multi: 0,
+                old_pm: 0,
+                new_pm: 0,
+                remain: 0,
+                need: 0,
+                total: 0,
+            },
+        ];
 
-    let oldBa = 0;
-    let newBa = 0;
-    let remainPrj = 0;
-    let needPrj = 0;
+        let oldPm = 0;
+        let newPm = 0;
+        let remainPrj = 0;
+        let needPrj = 0;
+        let total = 0;
 
-    function findMultiplier(project, handle) {
-      let new_handle = handle;
-      let mul = new_handle / project;
+        function findMultiplier(project, handle) {
+            let new_handle = handle;
+            let mul = new_handle / project;
 
-      while (mul < 1) {
-        new_handle = new_handle + handle;
-        mul = new_handle / project;
-      }
+            while (mul < 1) {
+                new_handle = new_handle + handle;
+                mul = new_handle / project;
+            }
 
-      return new_handle;
+            return new_handle;
+        }
+
+        for (let index in projectsArray) {
+            let multiplier = findMultiplier(projectsArray[index], projectHandling);
+
+            newPm = multiplier / projectHandling;
+            remainPrj = multiplier - projectsArray[index];
+            needPrj = remainPrj > 0 ? 1 : 0;
+
+            for (let i = 1; i < table.length; i++) {
+                newPm = Math.round(multiplier / projectsArray[index]) - needPrj;
+
+                let remain01 = projectHandling * newPm;
+                remainPrj = remain01 - (projectsArray[index] - table[i].remain);
+
+                needPrj = remainPrj > 0 ? 1 : 0;
+            }
+
+            for (let i = projectRotation; i < table.length; i++) {
+                let old_prep = table[i - i].new_pm - 1;
+                let old_prep_2 = table[i - i].new_pm - old_prep;
+
+                oldPm = table[i].need > 0 ? old_prep_2 + table[i].need : table[i].old_pm + table[i].new_pm;
+
+                newPm = multiplier / projectHandling - oldPm;
+
+                if (newPm < 0) {
+                    newPm = 0;
+                }
+
+                let new_remains = projectsArray[index] - table[i].remain;
+                let mul = multiplier / projectHandling;
+                let new_mul = mul - table[i].need;
+                let remain_main = new_mul * projectHandling;
+
+                remainPrj = remain_main - new_remains;
+
+                if (remainPrj < 0) {
+                    remainPrj = projectHandling + remainPrj;
+                }
+
+                needPrj = remainPrj > 0 ? 1 : 0;
+            }
+
+            total = total + newPm;
+
+            table.push({
+                projects: projectsArray[index],
+                multi: multiplier,
+                old_pm: oldPm,
+                new_pm: newPm,
+                remain: remainPrj,
+                need: needPrj,
+                total: total,
+            });
+        }
+
+        return table;
     }
 
-    for (let index in projectsArray) {
-      let multiplier = findMultiplier(projectsArray[index], projectHandling);
+    const data = findPM(projects, rotation, handle);
 
-      newBa = Math.round(multiplier / projectsArray[index]);
-      remainPrj = multiplier - projectsArray[index];
-      needPrj = remainPrj > 0 ? 1 : 0;
-
-      for (let i = 1; i < table.length; i++) {
-        newBa = needPrj;
-
-        let remain01 = projectHandling * newBa;
-        remainPrj = remain01 - (projectsArray[index] - table[i].remain);
-
-        needPrj = remainPrj > 0 ? 1 : 0;
-      }
-
-      for (let o = projectRotation; o < table.length; o++) {
-        oldBa = table[o].need;
-      }
-
-      for (let i = projectRotation + 1; i < table.length; i++) {
-        let old_prep = table[i - i].new_ba - 1;
-        let old_prep_2 = table[i - i].new_ba - old_prep;
-
-        oldBa = table[i].need > 0 ? old_prep_2 + table[i].need : old_prep_2;
-
-        if (table[i].remain == projectsArray[index]) {
-          oldBa = 1;
-        }
-
-        newBa = Math.ceil(multiplier / projectHandling) - oldBa;
-
-        if (newBa < 0) {
-          newBa = 0;
-        }
-
-        let new_remains = projectsArray[index] - table[i].remain;
-        let mul = multiplier / projectHandling;
-        let new_mul = mul - table[i].need;
-        let remain_main = new_mul * projectHandling;
-
-        remainPrj = remain_main - new_remains;
-
-        if (remainPrj < 0) {
-          newBa += 1;
-          remainPrj = projectHandling + remainPrj;
-        }
-
-        needPrj = remainPrj > 0 ? 1 : 0;
-      }
-
-      table.push({
-        projects: projectsArray[index],
-        multi: multiplier,
-        old_ba: oldBa,
-        new_ba: newBa,
-        remain: remainPrj,
-        need: needPrj,
-      });
-    }
-
-    return table;
-  }
-
-  const data = findBA(projects, rotation, handle);
-
-  const tableHead = `
+    const tableHead = `
       
           <tr>
               <th>Index</th>
@@ -119,25 +115,27 @@ function setConfig(projectss, rotationss, handlingss) {
               <th>New</th>
               <th>Remains</th>
               <th>Available</th>
+              <th>Total</th>
           </tr>
       `;
 
-  const html = data
-    .map((da, index) => {
-      return `
+    const html = data
+        .map((da, index) => {
+            return `
               <tr>
                   <td><b>${index}</b></td>
                   <td>${da.projects}</td>
                   <!-- <td>${da.multi}</td> -->
-                  <td>${da.old_ba}</td>
-                  <td>${da.new_ba}</td>
+                  <td>${da.old_pm}</td>
+                  <td>${da.new_pm}</td>
                   <td>${da.remain}</td>
                   <td>${da.need}</td>
+                  <td>${da.total}</td>
               </tr>
           `;
-    })
-    .join("");
+        })
+        .join('');
 
-  const htmlTable = document.querySelector("#maintable");
-  htmlTable.innerHTML = tableHead + html;
+    const htmlTable = document.querySelector('#maintable');
+    htmlTable.innerHTML = tableHead + html;
 }
