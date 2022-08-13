a1868673e7677b82dd4281fe6be194f69f03ed41
Bunleap Sorn <bunleap.sorn@gmail.com>
2022-08-13 10:40:35 +0700

    dynamic attrition rate for engineer

--- data/engineer/index.js
+++ data/engineer/index.js
@@ -1,63 +1,68 @@
-function setConfig(projectss, rotationss, handlingss) {
-  let config = {
-    projectsList: projectss
-      .replace(/(?:\r\n|\r|\n)/g, "")
-      .split(",")
-      .map(function (item) {
-        return parseInt(item);
-      }),
-    rotationss: parseInt(rotationss),
-    handlingss: parseInt(handlingss),
-  };
+function setConfig(projectss, rotationss, handlingss, attritionss) {
+    let config = {
+        projectsList: projectss
+            .replace(/(?:\r\n|\r|\n)/g, '')
+            .split(',')
+            .map(function (item) {
+                return parseInt(item);
+            }),
+        rotationss: parseInt(rotationss),
+        handlingss: parseInt(handlingss),
+        attritionss: parseInt(attritionss),
+    };
 
-  const projects = config.projectsList;
-  const rotation = config.rotationss;
-  const handle = config.handlingss;
+    const projects = config.projectsList;
+    const rotation = config.rotationss;
+    const handle = config.handlingss;
+    const attritions = config.attritionss;
 
-  function findEng(projectsArray, projectRotation, projectHandling) {
-    let table = [
-      {
-        projects: 0,
-        multi: 0,
-        old_eng: 0,
-        new_eng: 0,
-        total: 0,
-      },
-    ];
+    function findEng(projectsArray, projectRotation, projectHandling, attritionRate) {
+        let table = [
+            {
+                projects: 0,
+                multi: 0,
+                old_eng: 0,
+                new_eng: 0,
+                total: 0,
+            },
+        ];
 
-    let oldEng = 0;
-    let newEng = 0;
-    let total = 0;
+        let oldEng = 0;
+        let newEng = 0;
+        let total = 0;
 
-    for (let index in projectsArray) {
-      let resource = projectHandling * projectsArray[index];
-      newEng = resource;
+        for (let index in projectsArray) {
+            let resource = projectHandling * projectsArray[index];
+            newEng = resource;
 
-      for (let num = projectRotation; num < table.length; num++) {
-        oldEng = table[num - (projectRotation - 1)].new_eng;
-        newEng = resource - oldEng;
+            let attrition_Rate = attritionRate / 100;
 
-        if (newEng < 0) {
-          newEng = 0;
+            for (let num = projectRotation; num < table.length; num++) {
+                oldEng = table[num - (projectRotation - 1)].new_eng;
+                oldEng = Math.ceil(attrition_Rate * oldEng);
+                newEng = resource - oldEng;
+
+                if (newEng < 0) {
+                    newEng = 0;
+                }
+            }
+
+            total = total + newEng;
+
+            table.push({
+                projects: projectsArray[index],
+                old_eng: oldEng,
+                new_eng: newEng,
+                total: total,
+            });
         }
-      }
 
-      total = total + newEng;
-
-      table.push({
-        projects: projectsArray[index],
-        old_eng: oldEng,
-        new_eng: newEng,
-        total: total,
-      });
+        return table;
     }
 
-    return table;
-  }
+    const data = findEng(projects, rotation, handle, attritions);
 
-  const data = findEng(projects, rotation, handle);
-
-  const tableHead = `
+    const tableHead = `
         
             <tr>
                 <th>Index</th>
@@ -68,9 +73,9 @@ function setConfig(projectss, rotationss, handlingss) {
             </tr>
         `;
 
-  const html = data
-    .map((info, index) => {
-      return `
+    const html = data
+        .map((info, index) => {
+            return `
                 <tr>
                     <td><b>${index}</b></td>
                     <td>${info.projects}</td>
@@ -79,9 +84,9 @@ function setConfig(projectss, rotationss, handlingss) {
                     <td>${info.total}</td>
                 </tr>
             `;
-    })
-    .join("");
+        })
+        .join('');
 
-  const htmlTable = document.querySelector("#maintable");
-  htmlTable.innerHTML = tableHead + html;
+    const htmlTable = document.querySelector('#maintable');
+    htmlTable.innerHTML = tableHead + html;
 }
